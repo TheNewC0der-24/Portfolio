08b13391242c7837a1b3d613eaba9dd318fdd9c0
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-11-19 05:19:05 +0530

    Create Loading Screen

--- src/LoadingScreen/LoadingScreen.jsx
+++ src/LoadingScreen/LoadingScreen.jsx
@@ -4,7 +4,7 @@ import styles from './LoadingScreen.module.css';
 const LoadingScreen = () => {
     return (
         <div className={styles.loaderContainer}>
-            <h1 className={styles.loading}></h1>
+            <div className={styles.loading}></div>
             <h2 className="fw-bold">Loading...</h2>
         </div>
     )
