0d6b34bbea4e2548d565661e6061622e3252c847
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-11-20 08:23:33 +0530

    🐞 fix: 'send' is defined but never used

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import emailjs, { send } from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
 import styles from './Contact.module.css';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
