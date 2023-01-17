53740037a13091e7d39247df3e39f65f1c05f818
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-01-18 02:59:32 +0530

    Improve UI

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 // Image
-import underConstruction from '../../assets/Images/underConstruction.svg'
+import underConstruction from '../../assets/Images/blog.svg'
 
 // Social Links
 import SocialLinks from '../../subComponents/SocialLinks/SocialLinks';
@@ -24,16 +24,27 @@ const Blogs = () => {
             <ThemeProvider theme={lightTheme}>
                 <GlobalStyle />
                 <SocialLinks />
-                <div className="container text-center mt-3">
-                    <img src={underConstruction} alt="underConstruction" width={600} className="img-fluid" />
-
-                    <h1 className='mt-5 fw-bold'>Under</h1>
-                    <div className="d-flex justify-content-center mx-auto">
-                        <div className="progress" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style={{ width: "50%" }}>
-                            <div className="progress-bar progress-bar-striped progress-bar-animated" style={{ width: "65%", backgroundColor: "#6d2ae2" }}></div>
+                <div className="container">
+                    <div className='p-4'>
+                        <div className="alert alert-dismissible fade show" role="alert">
+                            <h4 className="alert-heading fw-bold">No Blogs to read!!</h4>
+                            <hr />
+                            <p className="mb-0">This page is Under Construction...</p>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div>
                     </div>
-                    <h1 className='fw-bold'>Construction</h1>
+
+                    <div className='text-center'>
+                        <img src={underConstruction} alt="underConstruction" width={300} className="img-fluid" />
+
+                        <h3 className='mt-3 fw-bold'>Under</h3>
+                        <div className="d-flex justify-content-center mx-auto">
+                            <div className="progress" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style={{ width: "50%" }}>
+                                <div className="progress-bar progress-bar-striped progress-bar-animated" style={{ width: "65%", backgroundColor: "#6d2ae2" }}></div>
+                            </div>
+                        </div>
+                        <h3 className='fw-bold'>Construction</h3>
+                    </div>
                 </div>
             </ThemeProvider>
         </>
