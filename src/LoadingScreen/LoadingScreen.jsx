59b73f80cf993591b9fa604720d8d5e872e80b07
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-01-18 00:14:18 +0530

    Change Loading Screen

--- src/LoadingScreen/LoadingScreen.jsx
+++ src/LoadingScreen/LoadingScreen.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './LoadingScreen.module.css';
 
+import logo from '../assets/Images/logo.svg';
+
 const LoadingScreen = () => {
     return (
         <div className={styles.loaderContainer}>
-            <div className={styles.loading}></div>
-            <h2 className="fw-bold">Loading...</h2>
+            <img src={logo} alt="react" className={styles.loading}></img>
         </div>
+
     )
 }
 
