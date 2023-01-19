f527baed48eeb0be8ef8c8080870bd4e8acd02e1
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-01-19 21:06:18 +0530

    Remove extra import

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 // Routing
