9ff3ca57b46ca3de216528b0a99a616e30dac878
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-02-07 04:16:11 +0530

    🐞 fix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styles from "./Navbar.module.css";
+import styles from "./navbar.module.css";
 
 import SoundBar from '../../subComponents/SoundBar';
 
