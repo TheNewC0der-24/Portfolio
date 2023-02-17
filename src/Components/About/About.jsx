76ac513a3df704c8dceb92097b277410407a339d
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-02-17 14:15:22 +0530

    🎈 perf

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -34,6 +34,7 @@ const GlobalStyle = createGlobalStyle`
   }
   
   .loadable {
+    width: 325px;
     overflow: hidden;
     margin: 0 auto;
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
@@ -53,7 +54,26 @@ const GlobalStyle = createGlobalStyle`
     50% {
       opacity: 0.3;
     }
-  }`;
+  }
+  
+  @media (max-width: 991.5px) and (min-width: 767.5px) {
+    .loadable {
+        width: 225px;
+    }
+  }
+
+  @media (max-width: 449.5px) {
+    .loadable {
+        width: 200px;
+    }
+  }
+
+  @media (max-width: 280.5px) {
+    .loadable {
+        width: 150px;
+    }
+  }
+  `;
 
 const About = () => {
 
@@ -79,7 +99,7 @@ const About = () => {
               <div className={`${styles.row} row g-5 mx-auto`}>
                 <div className="col-lg-5">
                   <h5 className={`${styles.me} text-center fw-bold mb-3`}>That's me <TiArrowForward className={`${styles.icon} fs-4`} /></h5>
-                  <div className={`${pulsing ? "pulse" : ""} d-flex justify-content-center loadable`} style={{ width: "325px", background: "#dee2e6", transform: "rotate(-10deg)" }}>
+                  <div className={`${pulsing ? "pulse" : ""} d-flex justify-content-center loadable`} style={{ background: "#dee2e6", transform: "rotate(-10deg)" }}>
                     <motion.img
                       initial={{ height: "auto", opacity: 0 }}
                       // style={{ height: imageLoading ? "5rem" : "auto" }}
@@ -92,14 +112,14 @@ const About = () => {
                           { opacity: { delay: 0.5, duration: 0.4 } })
                       }
                       onLoad={imageLoaded}
-                      width={300}
+                      width="100%"
                       src={about}
                       alt="profile"
-                      className="img-fluid"
+                      className={`${styles.img} img-fluid`}
                     />
                   </div>
 
-                  <div className='mt-5'>
+                  <div className='mt-5 text-center'>
                     <span className={styles.tag}>&lt;p&gt;</span>
                     <p className={styles.content}>
                       I'm obsessed with making things and even more obsessed with making things better.
@@ -109,12 +129,12 @@ const About = () => {
                 </div>
                 <div className={`${styles.mainContent} col-lg-7`}>
                   <div className={styles.tag}>&lt;h5&gt;</div>
-                  <h5>
+                  <p>
                     I am <span className={`${styles.main} text-danger`}>Bhavya Khurana</span>, and I'm a <span className={`${styles.main} text-warning`}>Frontend Developer</span> from <span className='text-success fw-bold'>Jaypee University of Engineering and Technology, Guna, India</span>. I am in my final year of my bachelors program, majoring in Computer Science. Furthermore, I have been working on web based development project with frontend related tech like <span className={`${styles.main} text-primary-emphasis`}>React.JS</span>. I am constantly working on improving my skills as a software engineer.
-                  </h5>
-                  <h5>
+                  </p>
+                  <p>
                     I am an incessant learner and a keen observer. Always ready to learn something new and enjoy bringing new ideas to life. I believe everything is an Art when we put our consciousness.
-                  </h5>
+                  </p>
                   <div className={styles.tag}>&lt;h5&gt;</div>
                   <div className='fw-bold fs-5 my-2' style={{ color: '#6d2ae2', fontFamily: 'Special Elite' }}>
                     <AnimatedText
@@ -138,7 +158,8 @@ const About = () => {
                     </AnimatedText>
                   </div>
                   <div className='mt-4 mb-3'>
-                    <a href={Resume} className={styles.downloadBtn} role="button" download>Download Resume <span></span></a>
+                    {/* <a href={Resume} className={styles.downloadBtn} role="button" download>Download Resume <span></span></a> */}
+                    <button className={styles.downloadBtn}>Download Resume <span></span></button>
                   </div>
                 </div>
               </div>
