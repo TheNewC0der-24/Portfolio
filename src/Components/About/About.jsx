64ddb4308fcb3b7fce55c1dbd8c8c7ba40e69e16
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-03-08 18:18:38 +0530

    Improve UI

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -16,13 +16,14 @@ import SocialLinks from '../../subComponents/SocialLinks/SocialLinks';
 // Theme
 import { darkTheme } from '../Themes';
 
-import Resume from '../../assets/Resume.pdf';
 
 // Icon
 import { TiArrowForward } from 'react-icons/ti';
 
 // Image
-import about from '../../assets/Images/about.svg';
+import about from '../../assets/Images/portrait.png';
+
+import Resume from '../../assets/Resume.pdf';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -34,16 +35,19 @@ const GlobalStyle = createGlobalStyle`
   }
   
   .loadable {
-    width: 325px;
+    width: 250px;
+    height: 250px;
     overflow: hidden;
     margin: 0 auto;
+    border-radius: 50%;
+    border: 5px solid #6d2ae2;
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
     0 2px 4px -1px rgba(0, 0, 0, 0.06);
   }
   
   img {
     display: block;
-    transform: rotate(-0.5deg);
+    // transform: rotate(-10deg);
   }
 
   @keyframes pulse {
@@ -59,18 +63,21 @@ const GlobalStyle = createGlobalStyle`
   @media (max-width: 991.5px) and (min-width: 767.5px) {
     .loadable {
         width: 225px;
+        height: 225px;
     }
   }
 
   @media (max-width: 449.5px) {
     .loadable {
         width: 200px;
+        height: 200px;
     }
   }
 
   @media (max-width: 280.5px) {
     .loadable {
         width: 150px;
+        height: 150px;
     }
   }
   `;
@@ -109,7 +116,7 @@ const About = () => {
               <div className={`${styles.row} row g-5 mx-auto`}>
                 <div className="col-lg-5">
                   <h5 className={`${styles.me} text-center fw-bold mb-3`}>That's me <TiArrowForward className={`${styles.icon} fs-4`} /></h5>
-                  <div className={`${pulsing ? "pulse" : ""} d-flex justify-content-center loadable`} style={{ background: "#dee2e6", transform: "rotate(-10deg)" }}>
+                  <div className={`${pulsing ? "pulse" : ""} d-flex justify-content-center loadable `} style={{ background: "#0e1313" }}>
                     <motion.img
                       initial={{ height: "auto", opacity: 0 }}
                       // style={{ height: imageLoading ? "5rem" : "auto" }}
@@ -140,7 +147,7 @@ const About = () => {
                 <div className={`${styles.mainContent} col-lg-7`}>
                   <div className={styles.tag}>&lt;h5&gt;</div>
                   <p>
-                    I am <span className={`${styles.main} text-danger`}>Bhavya Khurana</span>, and I'm a <span className={`${styles.main} text-warning`}>Frontend Developer</span> from <span className='text-success fw-bold'>Jaypee University of Engineering and Technology, Guna, India</span>. I am in my final year of my bachelors program, majoring in Computer Science. Furthermore, I have been working on web based development project with frontend related tech like <span className={`${styles.main} text-primary-emphasis`}>React.JS</span>. I am constantly working on improving my skills as a software engineer.
+                    I am <span className={`${styles.main} text-danger`}>Bhavya Khurana</span>, and I'm a <span className={`${styles.main} text-warning`}>Frontend Developer</span> from <span className='text-success fw-bold'>Jaypee University of Engineering and Technology, Guna, India</span>. I am in my final year of my bachelors program, majoring in Computer Science. Furthermore, I have been working on web based development project with frontend related tech like <span className={`${styles.main} text-primary-emphasis`}>React.JS</span> and <span className={`${styles.main} text-primary-emphasis`}>Angular</span>. I am constantly working on improving my skills as a software engineer.
                   </p>
                   <p>
                     I am an incessant learner and a keen observer. Always ready to learn something new and enjoy bringing new ideas to life. I believe everything is an Art when we put our consciousness.
