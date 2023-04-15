d235a1bb92bf61555eac215f1ba64e910d53a5c1
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-04-15 22:11:50 +0530

    Using react icons

--- src/subComponents/SocialLinks/SocialLinks.jsx
+++ src/subComponents/SocialLinks/SocialLinks.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
-import './SocialLinks.css';
+import styles from './SocialLinks.module.css';
 
-import { SiHashnode } from 'react-icons/si';
-import { FaFacebookSquare } from 'react-icons/fa';
+import { SiHashnode, SiMedium } from 'react-icons/si';
+import { FaFacebookSquare, FaGithub, FaInstagram, FaLinkedin, FaStackOverflow, FaTwitter } from 'react-icons/fa';
 
 import { motion } from 'framer-motion';
 
 const Line = styled(motion.span)`
-width : 2px;
-height : 8rem;
-background-color: #6d2ae2;
+    width : 2px;
+    height : 8rem;
+    background-color: #6d2ae2;
 `;
 
 const SocialLinks = () => {
     return (
         <>
-            <div className='icons'>
+            <div className={styles.icons}>
                 <motion.div
                     initial={{ transform: "scale(0)" }}
                     animate={{ scale: [0, 1, 1.5, 1] }}
                     transition={{ type: 'spring', duration: 1, delay: 1 }}
                 >
                     <a href="https://www.facebook.com/bhavya.khurana.399/" rel="noreferrer" target="_blank">
-                        {/* <i className="mb-3 fs-4 fa-brands fa-facebook-square"></i> */}
-                        <FaFacebookSquare className="mb-3 fs-4 fa-brands fa-facebook-square" />
+                        <FaFacebookSquare className={`${styles.socialIcon} mb-3 fs-4`} />
                     </a>
                 </motion.div>
 
@@ -34,7 +33,7 @@ const SocialLinks = () => {
                     transition={{ type: 'spring', duration: 1, delay: 1.2 }}
                 >
                     <a href="https://github.com/TheNewC0der-24" rel="noreferrer" target="_blank">
-                        <i className="mb-3 fs-4 fa-brands fa-github"></i>
+                        <FaGithub className={`${styles.socialIcon} mb-3 fs-4`} />
                     </a>
                 </motion.div>
 
@@ -44,7 +43,7 @@ const SocialLinks = () => {
                     transition={{ type: 'spring', duration: 1, delay: 1.4 }}
                 >
                     <a href="https://www.instagram.com/__khurana__._/" rel="noreferrer" target="_blank">
-                        <i className="mb-3 fs-4 fa-brands fa-instagram-square"></i>
+                        <FaInstagram className={`${styles.socialIcon} mb-3 fs-4`} />
                     </a>
                 </motion.div>
 
@@ -54,7 +53,7 @@ const SocialLinks = () => {
                     transition={{ type: 'spring', duration: 1, delay: 1.6 }}
                 >
                     <a href="https://www.linkedin.com/in/bhavyakhurana24/" rel="noreferrer" target="_blank">
-                        <i className="mb-3 fs-4 fa-brands fa-linkedin"></i>
+                        <FaLinkedin className={`${styles.socialIcon} mb-3 fs-4`} />
                     </a>
                 </motion.div>
 
@@ -64,7 +63,7 @@ const SocialLinks = () => {
                     transition={{ type: 'spring', duration: 1, delay: 1.8 }}
                 >
                     <a href="https://medium.com/@bhavyakhurana24" rel="noreferrer" target="_blank">
-                        <i className="mb-3 fs-4 fa-brands fa-medium"></i>
+                        <SiMedium className={`${styles.socialIcon} mb-3 fs-4`} />
                     </a>
                 </motion.div>
 
@@ -74,7 +73,7 @@ const SocialLinks = () => {
                     transition={{ type: 'spring', duration: 1, delay: 2 }}
                 >
                     <a href="https://hashnode.com/@BhavyaKhurana" rel="noreferrer" target="_blank">
-                        <SiHashnode className="mb-3 fs-4 fa-brands fa-hashnode" />
+                        <SiHashnode className={`${styles.socialIcon} mb-3 fs-4`} />
                     </a>
                 </motion.div>
 
@@ -84,7 +83,7 @@ const SocialLinks = () => {
                     transition={{ type: 'spring', duration: 1, delay: 2.2 }}
                 >
                     <a href="https://stackoverflow.com/users/16262496/bhavya-khurana" rel="noreferrer" target="_blank">
-                        <i className="mb-3 fs-4 fa-brands fa-stack-overflow"></i>
+                        <FaStackOverflow className={`${styles.socialIcon} mb-3 fs-4`} />
                     </a>
                 </motion.div>
 
@@ -94,7 +93,7 @@ const SocialLinks = () => {
                     transition={{ type: 'spring', duration: 1, delay: 2.4 }}
                 >
                     <a href="https://twitter.com/Bhavya06001699" rel="noreferrer" target="_blank">
-                        <i className="mb-3 fs-4 fa-brands fa-twitter"></i>
+                        <FaTwitter className={`${styles.socialIcon} mb-3 fs-4`} />
                     </a>
                 </motion.div>
 
@@ -109,12 +108,12 @@ const SocialLinks = () => {
                 <motion.a
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.9 }}
-                    className="fs-4 sayHi"
+                    className={`fs-4 ${styles.sayHi}`}
                     href='mailto:khuranabhavya24@gmail.com'>
-                    <span className='s'>S</span>
-                    <span className='a'>a</span>
-                    <span className='y'>y</span>
-                    <span className='hi ms-2'>Hi...</span>
+                    <span className={styles.s}>S</span>
+                    <span className={styles.a}>a</span>
+                    <span className={styles.y}>y</span>
+                    <span className={`${styles.hi} ms-2`}>Hi...</span>
                 </motion.a>
             </div>
         </>
