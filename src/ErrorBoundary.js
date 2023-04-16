ab3a1698688b5859575dd17b78cf4d495f10a967
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-04-16 21:58:06 +0530

    Improve UI

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,9 +1,22 @@
+import Error from "./assets/Images/Error.svg";
+
 function ErrorFallback({ error, resetErrorBoundary }) {
     return (
-        <div role="alert" className="container my-5">
-            <h3>Something went wrong:</h3>
-            <pre>{error.message}</pre>
-            <button className="btn btn-danger" style={{ borderRadius: "0px" }} onClick={resetErrorBoundary}>Try again</button>
+        <div className="container my-5">
+            <div className="d-flex justify-content-center">
+                <img className="img-fluid w-50" src={Error} alt="error" />
+            </div>
+            <div className="d-flex justify-content-between align-items-center">
+                <h1 className="mb-3">Something went wrong</h1>
+                <button className="btn" style={{ borderRadius: "0px", backgroundColor: "#6d2ae2", color: "#dee2e6" }} onClick={resetErrorBoundary}>Try again</button>
+            </div>
+            <p>It's look like you have stumbled out into the void.</p>
+            <p>Don't worry, I am here to help you.</p>
+            <div className="card">
+                <div className="card-body">
+                    <pre>{error.message}</pre>
+                </div>
+            </div>
         </div>
     )
 }
