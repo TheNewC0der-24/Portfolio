512b470ec1b346b7d5862756b7b00775d89e24d4
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-05-31 13:20:08 +0530

    Portfolio

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,11 +5,6 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 import { motion } from "framer-motion";
 
-// Particles
-import ParticleComponent from '../../subComponents/ParticleComponent';
-
-// import AnimatedText from 'react-animated-text-content';
-
 // Social Links
 import SocialLinks from '../../subComponents/SocialLinks/SocialLinks';
 
@@ -23,10 +18,12 @@ import { TiArrowForward } from 'react-icons/ti';
 import about from '../../assets/Images/portrait.svg';
 
 import Resume from '../../assets/Resume.pdf';
+import ParticleBackgroundAnimate from '../../subComponents/ParticleBackgroundAnimate/ParticleBackgroundAnimate';
 
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${props => props.theme.body};
+    color: ${props => props.theme.text};
   }
 
   .pulse {
@@ -39,15 +36,21 @@ const GlobalStyle = createGlobalStyle`
     overflow: hidden;
     margin: 0 auto;
     border-radius: 50%;
-    border: 5px solid #6d2ae2;
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
     0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    border: 5px solid #6d2ae2;
+    transition: all 0.3s ease-in-out;
   }
   
   img {
     display: block;
-    // transform: rotate(-10deg);
     z-index: 999;
+    filter: grayscale(100%);
+    transition: all 0.3s ease-in-out;
+  }
+  img:hover{
+    filter: grayscale(0%);
+    transform: scale(1.1);
   }
 
   @keyframes pulse {
@@ -79,8 +82,7 @@ const GlobalStyle = createGlobalStyle`
         width: 150px;
         height: 150px;
     }
-  }
-  `;
+  }`;
 
 const About = () => {
 
@@ -110,16 +112,19 @@ const About = () => {
       <ThemeProvider theme={darkTheme}>
         <GlobalStyle />
         <SocialLinks />
-        <ParticleComponent />
-        <div className={`${styles.container} container my-4`}>
+        <ParticleBackgroundAnimate />
+        <div className={`${styles.container} container`}>
           <h1 className={`${styles.title} text-center`}>.about <span style={{ color: "#6d2ae2" }}>Me()</span></h1>
-          <h6 className='text-center fw-bold' style={{ color: "#9a9ea1" }}>ALLOW ME TO INTRODUCE MYSELF</h6>
+          <h6 className='text-center fw-bold' style={{ color: "#dee2e6" }}>ALLOW ME TO INTRODUCE MYSELF</h6>
           <div className="container">
             <div className="container d-flex mt-5">
               <div className={`${styles.row} row g-5 mx-auto`}>
                 <div className="col-lg-5">
-                  <h5 className={`${styles.me} text-center fw-bold mb-3`}>That's me <TiArrowForward className={`${styles.icon} fs-4`} /></h5>
-                  <div className={`${pulsing ? "pulse" : ""} d-flex justify-content-center loadable `} style={{ background: "#0e1313" }}>
+                  <h5 className={`${styles.me} text-center fw-bold mb-3`}>
+                    That's me <TiArrowForward className={`${styles.icon} fs-4`} />
+                  </h5>
+
+                  <div className={`${pulsing ? "pulse" : ""} d-flex justify-content-center loadable `} style={{ background: "#0e1313", display: 'block' }}>
                     <motion.img
                       initial={{ height: "auto", opacity: 0 }}
                       // style={{ height: imageLoading ? "5rem" : "auto" }}
@@ -136,51 +141,37 @@ const About = () => {
                       src={about}
                       alt="profile"
                       className={`${styles.img} img-fluid`}
+                      data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Tooltip on bottom"
                     />
                   </div>
 
                   <div className='mt-5 text-center'>
                     <span className={styles.tag}>&lt;p&gt;</span>
-                    <p className={styles.content}>
+                    <p className={styles.content} style={{ color: "#6d2ae2" }}>
                       I'm obsessed with making things and even more obsessed with making things better.
                     </p>
                     <span className={styles.tag}>&lt;p&gt;</span>
+                    <div className='mt-4 mb-3'>
+                      <button onClick={handleDownload} className={styles.downloadBtn}>Download Resume<span></span></button>
+                    </div>
                   </div>
+
                 </div>
                 <div className={`${styles.mainContent} col-lg-7`}>
                   <div className={styles.tag}>&lt;h5&gt;</div>
                   <p>
-                    I am <span className={`${styles.main} text-danger`}>Bhavya Khurana</span>, and I'm a <span className={`${styles.main} text-warning`}>Frontend Developer</span> from <span className='text-success fw-bold'>Jaypee University of Engineering and Technology, Guna, India</span>. I am in my final year of my bachelors program, majoring in Computer Science. Furthermore, I have been working on web based development project with frontend related tech like <span className={`${styles.main} text-primary-emphasis`}>React.JS</span> and <span className={`${styles.main} text-primary-emphasis`}>Angular</span>. I am constantly working on improving my skills as a software engineer.
+                    I am <span className={`${styles.main} text-danger fs-5`}>Bhavya Khurana</span>, and I'm a <span className={`${styles.main} text-warning fs-5`}>Frontend Developer</span>. I have completed my bachelors program from <span className={`${styles.main} text-success fs-5`}>Jaypee University of Engineering and Technology, Guna, India</span> majoring in Computer Science. Furthermore, I have been working on web based development project with frontend related tech like <span className={`${styles.main} text-primary-emphasis fs-5`}>React.JS</span> and <span className={`${styles.main} text-primary-emphasis fs-5`}>Angular</span>. I am constantly working on improving my skills as a software engineer.
                   </p>
                   <p>
                     I am an incessant learner and a keen observer. Always ready to learn something new and enjoy bringing new ideas to life. I believe everything is an Art when we put our consciousness.
                   </p>
+                  <p>
+                    I am a creative and skilled Frontend Developer with a passion for building elegant and intuitive user interfaces. With over 1 year of experience, I have worked on various web projects for clients in industries. I have expertise in experience with responsive design, accessibility, and website optimization. I have completed several online certification courses. I am a problem-solver with excellent communication skills and the ability to work well under pressure. I am committed to delivering high-quality solutions that meet user needs and exceed client expectations.
+                  </p>
                   <div className={styles.tag}>&lt;h5&gt;</div>
-                  <div className='fw-bold fs-5 my-2' style={{ color: '#6d2ae2', fontFamily: 'Special Elite' }}>
-                    {/* <AnimatedText
-                      type="chars"
-                      animation={{
-                        x: '200px',
-                        y: '-20px',
-                        scale: 1.1,
-                        ease: 'ease-in-out',
-                      }}
-                      animationType="rifle"
-                      interval={0.06}
-                      duration={0.8}
-                      tag="p"
-                      className="animated-paragraph"
-                      includeWhiteSpaces
-                      threshold={0.1}
-                      rootMargin="20%"
-                    >
-                      Open to Opportunities related to Web Development.
-                    </AnimatedText> */}
-                  </div>
-                  <div className='mt-4 mb-3'>
-                    {/* <a href={Resume} className={styles.downloadBtn} role="button" download>Download Resume <span></span></a> */}
-                    <button onClick={handleDownload} className={styles.downloadBtn}>Download Resume <span></span></button>
-                  </div>
+                  <p className='fw-bold my-2 p-2' style={{ backgroundColor: '#dee2e6', color: '#6d2ae2', fontFamily: 'Special Elite' }}>
+                    Open to Opportunities related to Web Development.
+                  </p>
                 </div>
               </div>
             </div>
