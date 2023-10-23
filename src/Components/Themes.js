1fe9d8513dfc38fd97f5af9874b10447e6aa8454
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-10-23 17:59:44 +0530

    Change

--- src/Components/Themes.js
+++
@@ -1,9 +0,0 @@
-export const lightTheme = {
-    body: '#dee2e6',
-    text: '#0e1313'
-}
-
-export const darkTheme = {
-    body: '#0e1313',
-    text: '#6d2ae2'
-}
--- src/Helpers/formatData.js
+++
@@ -1,7 +0,0 @@
-export const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const day = date.getDate();
-    const month = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date);
-    const year = date.getFullYear();
-    return `${month} ${day}, ${year}`;
-};
