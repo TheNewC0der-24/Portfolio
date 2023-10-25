20e4661212b6eb88a42cb0a2c37436337d4af904
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-10-26 03:21:47 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 const manifestForPlugin = {
   registerType: "prompt",
-  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "maskable.png"],
   manifest: {
     short_name: "Bhavya Khurana",
     name: "Bhavya Khurana Portfolio",
