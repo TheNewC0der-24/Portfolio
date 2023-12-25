d2e3fe3267449112530ab93cb3e34b652467068d
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-12-25 21:18:03 +0530

    Remove Loader and Lazy Loading of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
 import './App.css';
-import React, { useState, lazy, Suspense, useEffect } from 'react';
+import React from 'react';
 
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallback from './ErrorBoundary';
 
-import LoadingScreen from './LoadingScreen/LoadingScreen';
 import NotFound from './NotFound/404NotFound';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const Navbar = lazy(() => import('./Layout/Header'));
-const Home = lazy(() => import('./Pages/Home/Home'));
-const About = lazy(() => import('./Pages/About/About'));
-const Resume = lazy(() => import('./Components/DisplayResume/DisplayResume'));
-const Education = lazy(() => import('./Pages/Education/Education'));
-const Interest = lazy(() => import('./Pages/Interest/Interest'));
-const Skills = lazy(() => import('./Pages/Skills/Skills'));
-const Work = lazy(() => import('./Pages/Work/Work'));
-const Experience = lazy(() => import('./Pages/Experience/Experience'));
-const Blogs = lazy(() => import('./Pages/Blogs/Blogs'));
-const BlogPost = lazy(() => import('./Pages/Blogs/BlogPost'));
-const Contact = lazy(() => import('./Pages/Contact/Contact'));
+import Navbar from './Layout/Header';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Resume from './Components/DisplayResume/DisplayResume';
+import Education from './Pages/Education/Education';
+import Interest from './Pages/Interest/Interest';
+import Skills from './Pages/Skills/Skills';
+import Work from './Pages/Work/Work';
+import Experience from './Pages/Experience/Experience';
+import Blogs from './Pages/Blogs/Blogs';
+import BlogPost from './Pages/Blogs/BlogPost';
+import Contact from './Pages/Contact/Contact';
 
 function App() {
   return (
@@ -41,59 +40,38 @@ function App() {
 
 function AppContent() {
 
-  const [loading, setLoading] = useState(false);
-
-  const location = useLocation();
-
-  useEffect(() => {
-    setLoading(true);
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, [location]);
-
   const isResumeRoute = location.pathname === '/resume';
 
   return (
-    <Suspense fallback={<LoadingScreen />}>
-      {
-        loading ? (
-          <LoadingScreen />
-        ) : (
-          <React.Fragment>
-            {!isResumeRoute && <Navbar />}
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/about" element={<About />} />
-              <Route exact path="/resume" element={<Resume />} />
-              <Route exact path="/work" element={<Work />} />
-              <Route exact path="/experience" element={<Experience />} />
-              <Route exact path="/blogs" element={<Blogs />} />
-              <Route exact path="/blog/:slug" element={<BlogPost />} />
-              <Route exact path="/education" element={<Education />} />
-              <Route exact path="/interest" element={<Interest />} />
-              <Route exact path="/skills" element={<Skills />} />
-              <Route exact path="/contact" element={<Contact />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-            <ToastContainer
-              position="top-right"
-              autoClose={2000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="colored"
-            />
-          </React.Fragment>
-        )
-      }
-    </Suspense>
+    <React.Fragment>
+      {!isResumeRoute && <Navbar />}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/resume" element={<Resume />} />
+        <Route exact path="/work" element={<Work />} />
+        <Route exact path="/experience" element={<Experience />} />
+        <Route exact path="/blogs" element={<Blogs />} />
+        <Route exact path="/blog/:slug" element={<BlogPost />} />
+        <Route exact path="/education" element={<Education />} />
+        <Route exact path="/interest" element={<Interest />} />
+        <Route exact path="/skills" element={<Skills />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </React.Fragment>
   );
 }
 
