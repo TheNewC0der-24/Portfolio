11685056539076a843f1caaa4d0990a99b06539f
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-12-28 02:58:33 +0530

    Code cleanup

--- src/Pages/Work/GridView.jsx
+++ src/Pages/Work/GridView.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
-// import { motion } from 'framer-motion';
 import {
     Grid,
     Card,
-    CardMedia,
     CardContent,
     CardHeader,
     Avatar,
@@ -12,7 +10,6 @@ import {
     IconButton,
     Collapse,
     Box,
-    // Skeleton
 } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { GoProject } from 'react-icons/go';
@@ -41,8 +38,6 @@ const GridView = ({ filteredProjects, searchTerm }) => {
     }, []);
 
     const [expanded, setExpanded] = useState({});
-    // const [isLoading, setIsLoading] = useState(true);
-    // const [hoveredCard, setHoveredCard] = useState(null);
 
     const handleExpandClick = (projectId) => {
         setExpanded((prevExpanded) => ({
@@ -50,23 +45,6 @@ const GridView = ({ filteredProjects, searchTerm }) => {
             [projectId]: !prevExpanded[projectId],
         }));
     };
-
-    // useEffect(() => {
-    //     const timer = setTimeout(() => {
-    //         setIsLoading(false);
-    //     }, 2000);
-
-    //     return () => clearTimeout(timer);
-    // }, []);
-
-
-    // const handleHoverStart = (cardId) => {
-    //     setHoveredCard(cardId);
-    // };
-
-    // const handleHoverEnd = () => {
-    //     setHoveredCard(null);
-    // };
 
     return (
         <>
@@ -78,64 +56,6 @@ const GridView = ({ filteredProjects, searchTerm }) => {
                 ) :
                     filteredProjects?.map((item) => (
                         <Grid item xs={12} sm={12} md={6} lg={4} key={item.id}>
-                            {/* <motion.div
-                                className={`card border-0 text-bg-dark ${hoveredCard === item.id ? 'hovered' : ''}`}
-                                onMouseEnter={() => handleHoverStart(item.id)}
-                                onMouseLeave={handleHoverEnd}
-                            >
-
-                                <img src={item.image} className="card-img img-fluid" alt={item.name} />
-                                <motion.div className="card-img-overlay bg-dark"
-                                    initial={{ opacity: 0, y: 10 }}
-                                    animate={{ opacity: hoveredCard === item.id ? 1 : 0, y: hoveredCard === item.id ? 0 : 10 }}
-                                    transition={{ duration: 0.3 }}
-                                >
-                                    <Card sx={{ borderBottom: '5px solid #6d2ae2' }}>
-                                        <CardHeader
-                                            avatar={
-                                                <Avatar sx={{ bgcolor: "#DFD8FD", color: "#6d2ae2" }}>
-                                                    <GoProject />
-                                                </Avatar>
-                                            }
-                                            title={item.name}
-                                            subheader={formatDate(item.created_at)}
-                                        />
-                                        <Box px={2}
-                                            sx={{
-                                                height: '100px',
-                                                overflow: 'hidden'
-                                            }}
-                                        >
-                                            <Typography variant="body2" color="text.secondary">
-                                                {item.description}
-                                            </Typography>
-                                            <div className='d-flex flex-wrap gap-2 mt-2'>
-                                                {
-                                                    item.topics.map((topic) => {
-                                                        return (
-                                                            <span key={topic} className='badge' style={{ backgroundColor: "#DFD8FD", color: "#6d2ae2" }}>{topic}</span>
-                                                        )
-                                                    })
-                                                }
-                                            </div>
-                                        </Box>
-                                        <CardActions disableSpacing>
-                                            <IconButton href={item.html_url} target="_blank" rel="noreferrer">
-                                                <FiGithub className='links' />
-                                            </IconButton>
-                                            {
-                                                item.homepage && (
-                                                    <IconButton href={item.homepage} target="_blank" rel="noreferrer">
-                                                        <FiExternalLink className='links' />
-                                                    </IconButton>
-                                                )
-                                            }
-                                        </CardActions>
-
-                                    </Card>
-                                </motion.div>
-                            </motion.div> */}
-
                             <Card sx={{ borderTop: '5px solid #6d2ae2' }}>
                                 <CardHeader
                                     avatar={
@@ -146,18 +66,12 @@ const GridView = ({ filteredProjects, searchTerm }) => {
                                     title={item.name}
                                     subheader={formatDate(item.created_at)}
                                 />
-                                {/* {
-                                    isLoading ? (
-                                        <Skeleton sx={{ height: 200 }} animation="wave" variant="rectangular" />
-                                    ) : ( */}
                                 <LazyLoadImage
                                     src={item.image}
                                     alt={item.name}
                                     effect="blur"
                                     className='img-fluid'
                                 />
-                                {/* )
-                                } */}
                                 <CardActions disableSpacing>
                                     <IconButton href={item.html_url} target="_blank" rel="noreferrer">
                                         <FiGithub className='links' />
